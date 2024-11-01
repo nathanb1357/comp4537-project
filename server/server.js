890cda24c561735a1972e612ce6f759914dcff26
Nathan Bartyuk <nathanb1357@gmail.com>
2024-11-01 14:00:42 -0700

    Trying to fix yet more bugs

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config({ path:'./server/.env' });
 
 const express = require('express');
 const db = require('./db/db');
+db.initializeTables();
 
 const { uploadImage, predictImage } = require('./controllers/api');
 const { register, login, resetPassword, verifyToken, authenticateToken} = require('./controllers/auth');
@@ -9,9 +10,6 @@ const { register, login, resetPassword, verifyToken, authenticateToken} = requir
 // Creates new express app
 const app = express();
 app.use(express.json());
-
-// Create db tables if none exist
-db.initializeTables();
 
 app.post('/auth/register', register);
 app.post('/auth/login', login);
