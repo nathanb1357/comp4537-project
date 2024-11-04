00cd6d0d4086bd2af242f6693a6387539e9625fc
Nathan Bartyuk <nathanb1357@gmail.com>
2024-11-03 17:45:02 -0800

    Implemented CORS in server

--- server/server.js
+++ server/server.js
@@ -2,15 +2,21 @@ require('dotenv').config({ path: './server/.env' });
 
 const express = require('express');
 const db = require('./db/db');
+const cors = require('cors');
 const { uploadImage, predictImage } = require('./controllers/api');
 const { register, login, resetPassword, verifyToken, authenticateToken} = require('./controllers/auth');
 
 async function startServer() {
     try {
-        await db.initializeTables();
+        db.initializeTables();
 
         const app = express();
         app.use(express.json());
+        app.use(cors({
+            origin: process.env.DOMAINS,
+            methods: ['GET', 'POST', 'OPTIONS'],
+            credentials: true
+        }));
 
         app.post('/auth/register', register);
         app.post('/auth/login', login);
@@ -34,48 +40,3 @@ async function startServer() {
 }
 
 startServer();
-
-
-// const express = require('express');
-// const db = require('./db/db');
-// db.initializeTables((err) => {
-//     if (err) {
-//         console.error('Error initializing tables:', err);
-//         process.exit(1); // Exit the application if DB initialization fails
-//     }
-// });
-
-// const { uploadImage, predictImage } = require('./controllers/api');
-// const { register, login, resetPassword, verifyToken, authenticateToken} = require('./controllers/auth');
-
-// // Creates new express app
-// const app = express();
-// app.use(express.json());
-
-// app.post('/auth/register', register);
-// app.post('/auth/login', login);
-// app.get('/auth/resetPassword/:email', resetPassword);
-// app.get('/auth/verifytoken/:token', verifyToken);
-// // app.post('/auth/resetPassword', changePassword);
-// // app.post('/api/predictImage', authenticateToken, uploadImage, predictImage);
-// // app.post('/api/getApiUsage', getApiUsage);
-
-
-// // Test to see if API working
-// app.get('/', (req, res) => {
-//     res.send('Welcome to the API server');
-// });
-
-// async function startServer() {
-//     try {
-//         db.initializeTables();
-//         app.listen(PORT, HOST, () => {
-//             console.log(`Server running on ${HOST}: port ${PORT}!`);
-//         });
-//     } catch (error) {
-//         console.error('Error initializing tables:', error);
-//         process.exit(1);
-//     }
-// }
-
-// startServer()
