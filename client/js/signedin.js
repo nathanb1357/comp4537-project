f41f6ee3048fc41077f1361939529701a4643774
sbeeve2023 <cgoss3@my.bcit.ca>
2024-11-04 17:00:53 -0800

    Added all users to table in admin page

--- client/js/signedin.js
+++ client/js/signedin.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   if (userData.expiry < Date.now()) {
     alert("Your session has expired. Please log in again.");
-    window.location.href = "index.html";
+    window.location.href = "register.html";
   }
 
   fetch(api + '/auth/userInfo/', {
@@ -28,8 +28,39 @@ document.addEventListener("DOMContentLoaded", function () {
   })
     .then(response => response.json())
     .then(data => {
+      if (data.user_role == "admin" && window.location.href.includes("signedinUser.html")) {
+        window.location.href = "signedinAdmin.html";
+      } else if (data.user_role == "user" && window.location.href.includes("signedinAdmin.html")) {
+        window.location.href = "signedinUser.html";
+      }
       document.getElementById("profileEmail").textContent = data.user_email || "N/A";
       document.getElementById("apiUsage").textContent = data.user_calls || "N/A";
+      if (data.user_role == "admin") {
+        fetch(api + '/auth/getAllUsers', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            "authorization": "Bearer " + userData
+          }
+        })
+          .then(response => response.json())
+          .then(data => {
+            let table = document.getElementById("userTable");
+            data.forEach(user => {
+              let row = table.insertRow(-1);
+              let cell1 = row.insertCell(0);
+              let cell2 = row.insertCell(1);
+              let cell3 = row.insertCell(2);
+              cell1.textContent = user.user_id;
+              cell2.textContent = user.user_email;
+              cell3.textContent = user.user_calls;
+            });
+          })
+          .catch(error => {
+            console.error("Error:", error);
+            alert("An error occurred. Please try again.");
+          });
+      }
     })
     .catch(error => {
       console.error("Error:", error);
